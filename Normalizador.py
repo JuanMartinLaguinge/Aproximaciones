fe93ba72bc4e2c4244d67ede4ce0d7c35bb3dbab
JuanMartinLaguinge <jlaguinge@itba.edu.ar>
2018-10-14 19:47:09 -0300

    Cambios de nombre para los tipos de filtro

--- Normalizador.py
+++ Normalizador.py
@@ -36,7 +36,7 @@ def main():
     #print("Favor de mandar los valores en el siguiente orden:")
     #print("As,Ap,Ws,Wp")bajos
     Wpn,Wan=Normalizacion('Pasa-banda',2e03,1e03,1)
-    print("Normalizate puto, El valor de Wpn es",Wpn,"y el de Wan es",Wan)
+    print("Normalizate, El valor de Wpn es",Wpn,"y el de Wan es",Wan)
    
 #Es necesario para poder ejecutar una funcion dentro del archivo
 if __name__ == "__main__":
