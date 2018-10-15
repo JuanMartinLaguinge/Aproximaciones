043e3770a7f204c75fc53127767ee0b74190c659
JuanMartinLaguinge <jlaguinge@itba.edu.ar>
2018-10-15 14:42:27 -0300

    Tentativo de la clase aproximacion

--- Aproximador.py
+++ Aproximador.py
@@ -1,14 +1,14 @@
 #Importo las funciones a utilizar
-import Normalizador
-import desnormalizacion
-import Chebycheff
-import Chebycheff2
-import butterworth
+from Normalizador import Normalizacion
+from desnormalizacion import desnormalizacion
+from Chebycheff import Chebyshev_Aprox
+from Chebycheff2 import Chebyshev2_Aprox
+from butterworth import butterworth
 
 def main():
     #print("Favor de mandar los valores en el siguiente orden:")
     #print("As,Ap,Ws,Wp")bajos
-    Wpn,Wan=Normalizador.Normalizacion('Pasa-banda',2e03,1e03,1)
+    Wpn,Wan=Normalizacion('Pasa-banda',2e03,1e03,1)
     print("Normalizate, El valor de Wpn es",Wpn,"y el de Wan es",Wan)
    
 #Es necesario para poder ejecutar una funcion dentro del archivo
