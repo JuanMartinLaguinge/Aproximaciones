e6f300afad0559944c4d857ae03bb2360943cb9d
JuanMartinLaguinge <jlaguinge@itba.edu.ar>
2018-10-15 15:20:26 -0300

    Tentativo de la clase aproximacion

--- Aproximador.py
+++ Aproximador.py
@@ -5,12 +5,40 @@ from Chebycheff import Chebyshev_Aprox
 from Chebycheff2 import Chebyshev2_Aprox
 from butterworth import butterworth
 
+class AproximadorFiltro:
+    #Defino el constructor de la clase en el que creo las variables a usar
+    def _init_(self):
+        self.Polos=[]
+        self.Zeros=[]
+    #Le pamos los datos necesarios para la aproximacion
+    def Datos(self,Tipo,Ap,As,Wp,Ws,Wp_mas=0,Ws_mas=0,Qmax=0,N=0,Nmin=0,Nmax=0):
+        self.Tipo=Tipo
+        self.Ws=Ws
+        self.Wp=Wp
+        self.As=As
+        self.Ap=Ap
+        self.Wp_mas=Wp_mas
+        self.Ws_mas=Ws_mas
+        self.Qmax=Qmax
+        self.N=N
+        self.Nmin=Nmin
+        self.Nmax=Nmax
+    #Se encarga de realizar la aproximacion dado los datos recibidos
+    def Aproximacion(self):
+        if self.Tipo=="Butterworth":
+            print("Butter")
+        elif self.Tipo=="Chebyshev I":
+            print("Cheby1")
+        elif self.Tipo=="Chebyshev II":
+            print("Cheby II")
+        else:
+            print("Bessel")
+
+
 def main():
-    #print("Favor de mandar los valores en el siguiente orden:")
-    #print("As,Ap,Ws,Wp")bajos
-    Wpn,Wan=Normalizacion('Pasa-banda',2e03,1e03,1)
-    print("Normalizate, El valor de Wpn es",Wpn,"y el de Wan es",Wan)
-   
+    Aprox=AproximadorFiltro()
+    Aprox.Datos("Butterworth",1,2,1,2)
+
 #Es necesario para poder ejecutar una funcion dentro del archivo
 if __name__ == "__main__":
     main()
