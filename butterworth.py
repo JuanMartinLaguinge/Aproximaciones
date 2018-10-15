67f2e90e0020a9354dd6378a71a5e2676754019e
gpanaggio <31071203+gpanaggio@users.noreply.github.com>
2018-10-15 16:56:23 -0300

    Butter completo

--- butterworth.py
+++ butterworth.py
@@ -2,14 +2,36 @@ import math
 import cmath
 from sympy import *
 
-def butterworth(Ap,wp,Aa,wa):
+def butterworth(Ap,As,Wpn,Wsn,N=0,Nmin=0,Nmax=0,rango=0):
+    #las variables wp_mas y ws_mas son para hacer bandas
+    #N es para que el usuario configure un orden fijo
+    #si Nmax no es sero se toma al N como orden minimo y Nmax como orden maximo
     E = sqrt(10**(Ap/10) - 1)
-    #print("E = ",E,"\n")
-    n = math.log10(sqrt(10**(Aa/10 - 1)) / E) / math.log10(wan)
-    n = int(n) + 1
-    #print("n = ",n,"\n")
+    n = math.log10(sqrt(10**(As/10) - 1) / E) / math.log10(Wsn)
+
+    if(N == 0):              #no hay orden fijo
+        if(Nmax == 0):      #no hay rango de orden
+            if(n == int(n)):
+                n = int(n)
+            else:
+                n = int(n) + 1
+        else:
+            if(n < Nmin):
+                n = Nmin
+            elif(n > Nmax):
+                n = Nmax
+            if(n == int(n)):
+                n = int(n)
+            else:
+                n = int(n) + 1
+    else:
+        n = N
+
+    if(rango != 0):      #el usuario quiere un rango de aproximacion de 1 a 100
+        Ep = sqrt(10**(As/10) - 1)/(Wsn**n)
+        E = E + (rango/100)*(Ep - E)
+
     radio = math.pow(E,-1/n)
-    #print("radio = ",radio,"\n")
     ang = math.pi/n
     ceros = []
     polos = []
@@ -20,4 +42,5 @@ def butterworth(Ap,wp,Aa,wa):
         a = cmath.rect(radio, phase_ini + k*ang)
         polos.append(round(re(a),6) + I*round(im(a),6))
         print("polo = ",polos[k],"\n")
+
     return ceros,polos
