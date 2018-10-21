e4a49d8cda9f2ade0caf463af1bee2315a7cfa0c
JuanMartinLaguinge <jlaguinge@itba.edu.ar>
2018-10-21 15:49:20 -0300

    Terminado la funcion de Bessel

--- Bessel.py
+++ Bessel.py
@@ -14,19 +14,12 @@ def PoliBessel (N):
         B=sympy.expand ( (2*N-1)*PoliBessel(N-1)+(s**2)*PoliBessel(N-2))
     return B
 
-#Se encarga de realizar la normalizacion para bessel
-def NormaBessel(Retardo,Wrg):
-    #Normalizo la frecuencia de paso
-    Wrgn=Retardo*Wrg
-    return Wrgn
-
 '''Se encarga de devolver los coeficientes de la funcion tranferencia de Bessel
 Recibe el Retardo de grupo querido en segundos, la frecuencia a la cual el retardo de grupo
 debe superar una cierta tolerancia y la tolerancia es Porcentaje/100
 Por ultimo,el Nmax por predeterminado es 15 para evitar un fitro muy grande'''
 def AproxBessel(Retardo,Wrg,Tol,N=0,Nmin=0,Nmax=15):
     s=sympy.Symbol('s')
-    Wn=NormaBessel(Retardo,Wrg)
     if N == 0:
         #Trato de obtener la funcion de bessel que cumpla con lo pedido
         N=1
@@ -35,8 +28,8 @@ def AproxBessel(Retardo,Wrg,Tol,N=0,Nmin=0,Nmax=15):
             N=Nmin
         while (N < Nmax) and (Nmin<=N):
             Polinomio = PoliBessel(N)
-            Tranferencia = Polinomio.subs(s,0)/Polinomio.subs(s,1j*Wn)
-            Temp= ( ( Wn**(2*N) ) * (abs(Tranferencia)**2) )/ (Polinomio.subs(s,0)**2)
+            Tranferencia = Polinomio.subs(s,0)/Polinomio.subs(s,1j*Wrg)
+            Temp= ( ( Wrg**(2*N) ) * (abs(Tranferencia)**2) )/ (Polinomio.subs(s,0)**2)
             if (Temp<Tol):
                 print(Temp)
                 break
