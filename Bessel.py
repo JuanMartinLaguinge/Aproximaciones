39b6889c249c9ff52e0b97f3c0e3ec5069fe43a1
JuanMartinLaguinge <jlaguinge@itba.edu.ar>
2018-10-21 12:52:04 -0300

    Falta los Q

--- Bessel.py
+++ Bessel.py
@@ -20,28 +20,41 @@ def NormaBessel(Retardo,Wrg):
     return Wrgn
 
 '''Se encarga de devolver los coeficientes de la funcion tranferencia de Bessel
-Recibe el Retardo de grupo querido, la freceuncai a la cual el retardo de grupo
-debe superar una cierta tolerancia y la tolerancia en porcentaje'''
-def AproxBessel(Retardo,Wrg,Tol):
+Recibe el Retardo de grupo querido, la frecuencia a la cual el retardo de grupo
+debe superar una cierta tolerancia y la tolerancia es Porcentaje/100
+Por ultimo,el Nmax por predeterminado es 15 para evitar un fitro muy grande'''
+def AproxBessel(Retardo,Wrg,Tol,N=0,Nmin=0,Nmax=15):
     s=sympy.Symbol('s')
     Wn=NormaBessel(Retardo,Wrg)
-    #Trato de obtener la funcion de bessel que cumpla con lo pedido
-    N=1
-    print("Frecuencia de paso normalizada es ",Wn)
-    while N < 15:
+    if N == 0:
+        #Trato de obtener la funcion de bessel que cumpla con lo pedido
+        N=1
+        #Si tengo un valor de Nmin voy a empezar desde ahi
+        if Nmin != 0:
+            N=Nmin
+        while (N < Nmax) and (Nmin<=N):
+            Polinomio = PoliBessel(N)
+            Tranferencia = Polinomio.subs(s,0)/Polinomio.subs(s,1j*Wn)
+            Temp= ( ( Wn**(2*N) ) * (abs(Tranferencia)**2) )/ (Polinomio.subs(s,0)**2)
+            if (Temp<Tol):
+                print(Temp)
+                break
+            else:
+                N+=1
+        #Chequeo para ver si estoy en el Nmax
+        if N == Nmax:
+            Polinomio = PoliBessel(N)
+            Tranferencia = Polinomio.subs(s,0)/Polinomio.subs(s,1j*Wn)
+            print("Se alcanzo el limite")
+        elif N == Nmin:
+            print("Se alconzo el minimo")
+    else:
         Polinomio = PoliBessel(N)
         Tranferencia = Polinomio.subs(s,0)/Polinomio.subs(s,1j*Wn)
-        Temp= ( ( Wn**(2*N) ) * (abs(Tranferencia)**2) )/ (Polinomio.subs(s,0)**2)
-        if (Temp<Tol):
-            print(Temp)
-            break
-        else:
-            N+=1
-    return N,Polinomio    
-import math
+    return N,Polinomio
 
 def main():
-    print(AproxBessel(10e-03,600,0.20))
+    print(AproxBessel(10e-03,600,0.20,6,2,7))
 
 if __name__ == "__main__":
     main()
