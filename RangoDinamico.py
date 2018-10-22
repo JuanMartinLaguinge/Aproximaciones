9b7342d19558200b2703d68a7db4151c393fd0c6
JuanMartinLaguinge <jlaguinge@itba.edu.ar>
2018-10-22 11:05:00 -0300

    Faltan pulir detalles

--- RangoDinamico.py
+++ RangoDinamico.py
@@ -3,14 +3,14 @@ from scipy import signal
 
 '''Quiero recibir los Numeradores y Denominadores de la funcion transferencia y
  espero resibir la tension de saturacion(Vmax) y la de ruido (Vmin) en Volts'''
-def RangoDinamico(Num,Den,Vmax=0,Vmin=0):
+def RangoDinamico(Num,Den,W,Vmax=0,Vmin=0):
     if Vmax==0:
         Vmax=10
     if Vmin==0:
         Vmin=10e-03
     RangoDin=20*math.log10(Vmax/Vmin)
     Transferencia = signal.lti(Num,Den)
-    W,Magnitud,Fase =signal.bode(Transferencia)
+    W,Magnitud,Fase =signal.bode(Transferencia,W)
     #Me desago de las variable que no uso
     W=0
     Fase=0
@@ -20,15 +20,15 @@ def RangoDinamico(Num,Den,Vmax=0,Vmin=0):
     Maxaux=max(Magnitud)
     Minaux=min(Magnitud)
     if Maxaux > MaxMagnitud:
-        MaxMagnitud=Maxaux
+        MaxMagnitud=10**(Maxaux/20)
     if Minaux < MinMagnitud:
-        MinMagnitud=Minaux
+        MinMagnitud=10**(Minaux/20)
     PerdidaRangoDin=20*math.log10(MaxMagnitud/MinMagnitud)
     RangoDin-=PerdidaRangoDin
     return RangoDin
 
 def main():
-    #RangoDinamico([])
+    RangoDinamico([1196.00631],[1, 7.77, 1196.0063],13.5,10e-03)
 #Es necesario para poder ejecutar una funcion dentro del archivo
 if __name__ == "__main__":
     main()
