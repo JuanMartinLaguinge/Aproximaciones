9fb7f9c3092c0b4ebe46515613c4c3cd1af2d032
gpanaggio <31071203+gpanaggio@users.noreply.github.com>
2018-10-24 15:35:19 -0300

    classEtapa - Version final

--- classEtapa.py
+++ classEtapa.py
@@ -5,10 +5,13 @@ import cmath
 class Etapa:
     H = signal.TransferFunction(1,[1,0])              #la funcion transerencia
     Q = 0
-    fo = 0
+    fo = 0 
     sel = 0             #un booleano que nos dice si la etapa esta seleccionada
+    Gmax = 0
 
     def loadData(self):
         if(len(self.H.poles) == 2):
-            self.Q = abs(self.H.poles[0])/(2*self.H.poles[0].real())
+            self.Q = abs(self.H.poles[0])/(abs(2*self.H.poles[0].real))
             self.fo = abs(self.H.poles[0])/(2*math.pi)
+            w,mag,pha = signal.bode(H)
+            self.Gmax = 10**(max(mag)/20)
